d04b6809ba6ae9a37f3e394cbbe84870a3b8bd65
= <=>
2021-11-06 13:12:16 +0000

    Zoo App Complete

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Enterprise_Apps/Zoo App/Assets/Scripts/CardView.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour
+{
+    public Text title, description, exhibit;
+    public Image animalImg;
+
+    public CardModel[] cards;
+
+    public void DisplayCard(int card)
+    {
+        title.text = cards[card].title;
+        description.text = cards[card].description;
+        exhibit.text = cards[card].exhibit;
+        animalImg.sprite = cards[card].animalImg;
+    }
+}
